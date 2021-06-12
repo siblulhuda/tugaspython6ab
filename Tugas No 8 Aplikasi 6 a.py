c8e80615607fbf0a24b29eb1965643bdb81ad850
siblulhuda <79742636+siblulhuda@users.noreply.github.com>
2021-06-12 13:36:04 +0700

    contoh screen shoot dari pak kukuh y

---
+++ Tugas No 8 Aplikasi 6 a.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 06:18:22 2021
+
+@author: Dell
+"""
+
+print("Input Jumlah Pembelian Printer Epson T20")
+print("----------------------------------------")
+print("Harga 1 Printer Rp 660.000,- ")
+#h = input("Masukkan Jumlah Pembelian Printer =  ")
+h = 660000
+i = input("Masukkan Harga Per Satuan =  ")
+n = int(h)
+m = int(i)
+print("Rp") 
+print(n * m)
---
+++ Tugas No 8 Aplikasi 6 b.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 23:36:48 2021
+
+@author: Dell
+"""
+
+print("Input Jumlah Pembelian Printer Epson T20")
+print("Jika nilai Pembelian diatas Rp 1.5 juta , diskon 15 %")
+print("----------------------------------------")
+print("Harga 1 Printer Rp 660.000,- ")
+#h = input("Masukkan Jumlah Pembelian Printer =  ")
+h = 660000
+i = input("Masukkan Harga Per Satuan =  ")
+j = 
+n = int(h)
+m = int(i)
+x = int()
+print("Rp") 
+x = (n * m)
+print(status)
+    
